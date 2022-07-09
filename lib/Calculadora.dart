028c034f8c406752475492378bb6d8df68741195
Felipe Lobinha <felipelboss@gmail.com>
2022-07-09 17:29:40 -0300

    4 operações prontas

--- lib/Calculadora.dart
+++ lib/Calculadora.dart
@@ -19,7 +19,7 @@ class CalculadoraState extends State<Calculadora> {
           children: <Widget>[
             //Texto que mostra resultado do cálculo
             new Text(
-              "Resultado: VARIAVEL_RESULTADO",
+              "Resultado: $resultado",
               style: new TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
@@ -29,18 +29,163 @@ class CalculadoraState extends State<Calculadora> {
             new TextField(
               keyboardType: TextInputType.number,
               decoration: new InputDecoration(hintText: "Informe o valor 1"),
-              //controller: t1,
+              controller: t1,
             ),
-            //CAMPO DE TEXO (input do valor 2)
+            //CAMPO DE TEXTO (input do valor 2)
             new TextField(
               keyboardType: TextInputType.number,
               decoration: new InputDecoration(hintText: "Informe o falor 2"),
-              //controller: t2,
-            )
+              controller: t2,
+            ),
+            //Espaçamento depois dos inputs
+            new Padding(padding: const EdgeInsets.only(top: 20)),
+
+            //Nova linha(row)
+
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  //child: new Text("+"),
+                  child: new Text(
+                    "+",
+                    style: new TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  color: Colors.deepOrangeAccent,
+                  onPressed:
+                      somar, //Se tornou uma palavra reservada por conta do setState
+                )
+              ],
+            ),
+
+            new Padding(padding: const EdgeInsets.only(top: 20)),
+
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text(
+                    "-",
+                    style: new TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  color: Colors.deepOrangeAccent,
+                  onPressed:
+                      subtrair, //Se tornou uma palavra reservada por conta do setState
+                )
+              ],
+            ),
+
+            new Padding(padding: const EdgeInsets.only(top: 20)),
+
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text(
+                    "*",
+                    style: new TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  color: Colors.deepOrangeAccent,
+                  onPressed:
+                      multiplicar, //Se tornou uma palavra reservada por conta do setState
+                )
+              ],
+            ),
+
+            new Padding(padding: const EdgeInsets.only(top: 20)),
+
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text(
+                    "/",
+                    style: new TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  color: Colors.deepOrangeAccent,
+                  onPressed:
+                      dividir, //Se tornou uma palavra reservada por conta do setState
+                )
+              ],
+            ),
+
+            //Espaçamento depois dos botões
+            new Padding(padding: const EdgeInsets.only(top: 20)),
+
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new MaterialButton(
+                    child: new Text("Clear"),
+                    color: Colors.grey,
+                    onPressed: limpar)
+              ],
+            ),
           ],
         ),
       ),
     );
   } //Fecha o método build
+
+  //Atributos
+  dynamic num1;
+  dynamic num2;
+  dynamic resultado = 0;
+
+  TextEditingController t1 = new TextEditingController(text: "");
+  TextEditingController t2 = new TextEditingController(text: "");
+
+  //Métodos
+  void somar() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      resultado = num1 + num2;
+    });
+  }
+
+  void subtrair() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      resultado = num1 - num2;
+    });
+  }
+
+  void multiplicar() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      resultado = num1 * num2;
+    });
+  }
+
+  void dividir() {
+    setState(() {
+      num1 = double.parse(t1.text);
+      num2 = double.parse(t2.text);
+      resultado = num1 / num2;
+    });
+  }
+
+  void limpar() {
+    setState(() {
+      t1.text = "";
+      t2.text = "";
+      resultado = 0;
+    });
+  }
 }//Fecha a classe CalculadoraState
 //NADA DEVE SER ADD DEPOIS DO RETURN, POIS NÃO SERÁ LIDO
